9954ecfc6b08a4cc922ab0d8a7b071973bd42d11
jvsegarra <jvsegarra@gmail.com>
2016-10-09 23:04:44 +0200

    get contacts use case tests

--- src/test/kotlin/Unit/Domain/GetContactsUseCaseTest.kt
+++ src/test/kotlin/Unit/Domain/GetContactsUseCaseTest.kt
@@ -1,4 +1,31 @@
 package Unit.Domain
 
-class GetContactsUseCaseTest {
+import Domain.Agenda
+import Domain.AgendaRepository
+import Domain.Contact
+import Domain.GetContactsUseCase
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.only
+import com.nhaarman.mockito_kotlin.verify
+import io.kotlintest.specs.FlatSpec
+
+class GetContactsUseCaseTest : FlatSpec() {
+    init {
+        "GetContactsUseCase.execute" should "return the contacts from the agenda repository" {
+            val agenda = Agenda("test")
+            agenda.addContact(Contact("", "", ""))
+            agenda.addContact(Contact("", "", ""))
+
+            val agendaRepositoryMock = mock<AgendaRepository>() {
+                on { get() } doReturn agenda
+            }
+
+            val getContactsUseCase = GetContactsUseCase(agendaRepositoryMock)
+            val result = getContactsUseCase.execute()
+
+            verify(agendaRepositoryMock).get()
+            result.contacts shouldEqual agenda.contacts
+        }
+    }
 }
